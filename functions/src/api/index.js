22116c868db35b54dda6e827ff4f1a5cd3ed3f9e
Mounir BAALI <mounir.baali@outlook.fr>
2024-07-23 12:49:45 +0100

    Update index.js

--- functions/src/api/index.js
+++ functions/src/api/index.js
@@ -1,22 +1,22 @@
 const app = require("express")();
 
-const createMatch = require("./createMatch/routes");
-const notificationsService = require("./notificationsService/routes");
-const supportService = require("./support/routes");
-const matchrPogression = require("./matchProgression/routes");
-const userHandler = require("./users/routes");
-const result = require("./createMatch/routes/resultRoutes");
-const location = require("./createMatch/routes/locationRoutes");
+// const createMatch = require("./createMatch/routes");
+// const notificationsService = require("./notificationsService/routes");
+// const supportService = require("./support/routes");
+// const matchrPogression = require("./matchProgression/routes");
+// const userHandler = require("./users/routes");
+// const result = require("./createMatch/routes/resultRoutes");
+// const location = require("./createMatch/routes/locationRoutes");
 
 
 
-app.use("/createMatch", createMatch);
-app.use("/location", location);
-app.use("/progression", matchrPogression);
-app.use("/notifications", notificationsService);
-app.use("/support", supportService);
-app.use("/users", userHandler);
-app.use("/result", result);
+// app.use("/createMatch", createMatch);
+// app.use("/location", location);
+// app.use("/progression", matchrPogression);
+// app.use("/notifications", notificationsService);
+// app.use("/support", supportService);
+// app.use("/users", userHandler);
+// app.use("/result", result);
 
 
 
